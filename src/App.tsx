af3062593dcedc6e39a96de8695b24d1adae736a
Steven Yang <yangshixin0629@qq.com>
2024-03-09 15:11:47 -0800

    Added BeforeRouterEnter for private route.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,50 @@
-import { useRoutes } from "react-router-dom";
+import { useRoutes, useLocation, useNavigate } from "react-router-dom";
 import routes from "@/router/routes";
+import { useEffect } from "react";
 
-function App() {
+const ToHomePage = () => {
+  const navigateTo = useNavigate();
+  useEffect(() => {
+    navigateTo("/page1");
+  }, []);
+  return <></>;
+};
+
+const ToLoginPage = () => {
+  const navigateTo = useNavigate();
+  useEffect(() => {
+    navigateTo("/login");
+  }, []);
+  return <></>;
+};
+
+const BeforeRouterEnter = () => {
+  const currentPath = useLocation();
+
   const outlet = useRoutes(routes);
 
-  return <>{outlet}</>;
+  const isAuthenticated = false;
+  /*
+  User Authentication Rules
+  1. if authenticated, and visti /login page, then direct to /home
+  2. if not authenticated, and visit other pages except /login page, direct to /login page.
+  3. other operations keep normal
+  */
+  if (currentPath.pathname === "/login" && isAuthenticated) {
+    return <ToHomePage />;
+  }
+  if (currentPath.pathname !== "/login" && !isAuthenticated) {
+    return <ToLoginPage />;
+  }
+
+  return outlet;
+};
+function App() {
+  return (
+    <>
+      <BeforeRouterEnter />
+    </>
+  );
 }
 
 export default App;
