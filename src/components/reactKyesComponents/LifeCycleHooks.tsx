6db9e012a3ad5a0739e152ecea8c8cf751f6c9e8
StevenYangSX <yangshixin1206@gmail.com>
2024-04-30 00:53:26 -0700

    Added Class component life cycle explanition part.

---
+++ src/components/reactKyesComponents/LifeCycleHooks.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LifeCycleHooks = () => {
+  return <div>LifeCycleHooks</div>;
+};
+
+export default LifeCycleHooks;
---
+++ src/pages/reactKeys/ReactLifeCycle.tsx
@@ -0,0 +1,120 @@
+import { Component } from "react";
+import { Divider } from "antd";
+import { Alert } from "antd";
+import LifeCycleHooks from "@/components/reactKyesComponents/LifeCycleHooks";
+import BasicCounter from "@/components/reactKyesComponents/BasicCounter";
+interface CounterState {
+  count: number;
+  consturctorAlert: boolean;
+  componentDidMountAlert: boolean;
+  getDerivedStateFromPropsAlert: boolean;
+}
+
+export default class ReactLifeCycle extends Component<{}, CounterState> {
+  //1. The Mounting Phase
+  // --1. Call consturctor() first.
+  // --2. Call getDerivedStateFromProps()
+  // --3. Call render() method
+  // --4. Call componentDidMount()
+
+  //2. The Updating Phase
+  // --1. Call getDerivedStateFromProps()
+  // --2. Call shouldComponentUpdate()
+  // --3. Call getSnapshotBeforeUpdate()
+  // --4. Call componentDidUpdate()
+  //3. The Unmounting Phase
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      count: 0,
+      consturctorAlert: true,
+      componentDidMountAlert: false,
+      getDerivedStateFromPropsAlert: false,
+    };
+  }
+
+  incrementCount = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+    }));
+  };
+
+  decrementCount = () => {
+    this.setState((prevState) => ({
+      count: prevState.count - 1,
+    }));
+  };
+
+  componentDidMount() {
+    this.setState({ componentDidMountAlert: true });
+  }
+
+  static getDerivedStateFromProps(_: any, state: any) {
+    return { ...state, getDerivedStateFromPropsAlert: true };
+  }
+
+  /**
+   * @deprecated
+   * @param nextProps
+   * @param nextState
+   */
+  //   componentWillUpdate(nextProps: any, nextState: any) {
+  //     console.log("componentWillUpdate called....", nextProps, nextState);
+  //   }
+
+  render() {
+    return (
+      <>
+        {this.state.consturctorAlert ? (
+          <Alert
+            type="success"
+            message="Class component constructor gets called."
+            banner
+            closable
+          />
+        ) : null}
+
+        {this.state.getDerivedStateFromPropsAlert ? (
+          <Alert type="success" message="getDerivedStateFromProps() gets called." banner closable />
+        ) : null}
+
+        {this.state.componentDidMountAlert ? (
+          <Alert type="success" message="componentDidMount() gets called." banner closable />
+        ) : null}
+        <h2
+          style={{
+            margin: "10px",
+            marginLeft: "auto",
+            marginRight: "auto",
+            display: "flex",
+            width: "300px",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+          }}
+        >
+          Parent Component : Ugly Counter
+        </h2>
+        <div
+          style={{
+            margin: "10px",
+            marginLeft: "auto",
+            marginRight: "auto",
+            display: "flex",
+            width: "300px",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+          }}
+        >
+          <button onClick={this.decrementCount}>-</button>
+          <span>{this.state.count}</span>
+          <button onClick={this.incrementCount}>+</button>
+        </div>
+
+        <Divider />
+        <BasicCounter count={this.state.count} />
+        <Divider />
+        <LifeCycleHooks />
+      </>
+    );
+  }
+}
